8292b35285aea3e52c1f9ae64147d260e638feab
Harrison <harrisonhough4@gmail.com>
2022-03-24 12:00:51 +0200

    added test logic

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
-import * as React from 'react'
-import { WebView } from 'react-native-webview'
+import * as React from "react";
+import { WebView } from "react-native-webview";
 
 export default class App extends React.Component {
   render() {
     return (
       <WebView
-        source={{ uri: 'https://demo.readyplayer.me/avatar' }} // Change the source uri to your custom subdomain
+        source={{ uri: "https://demo.readyplayer.me/avatar?frameapi" }} // Change the source uri to your custom subdomain
         onMessage={(event) => {
-          alert(
-            `Avatar 3D model can be downloaded from: ${event.nativeEvent.data}`
-          )
+          const json = parse(event);
+
+          if (json?.source !== "readyplayerme") {
+            return;
+          }
+
+          // Susbribe to all events sent from Ready Player Me once frame is ready
+          if (json.eventName === "v1.frame.ready") {
+            frame.contentWindow.postMessage(
+              JSON.stringify({
+                target: "readyplayerme",
+                type: "subscribe",
+                eventName: "v1.**",
+              }),
+              "*"
+            );
+          }
+
+          // Get avatar GLB URL
+          if (json.eventName === "v1.avatar.exported") {
+            console.log(`Avatar URL: ${json.data.url}`);
+            document.getElementById(
+              "avatarUrl"
+            ).innerHTML = `Avatar URL: ${json.data.url}`;
+            document.getElementById("frame").hidden = true;
+            alert(`Avatar 3D model can be downloaded from: ${json.data.ul}`);
+          }
+
+          // Get user id
+          if (json.eventName === "v1.user.set") {
+            console.log(
+              `User with id ${json.data.id} set: ${JSON.stringify(json)}`
+            );
+          }
+
+          function parse(event) {
+            try {
+              return JSON.parse(event.data);
+            } catch (error) {
+              return null;
+            }
+          }
         }}
       />
-    )
+    );
   }
 }
