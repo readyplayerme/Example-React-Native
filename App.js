1fa932cdbaf64e1bd27cdb44019266e552d6ec29
Harrison <harrisonhough4@gmail.com>
2022-03-25 10:38:56 +0200

    added basic support for new new postmessage

--- App.js
+++ App.js
@@ -1,55 +1,50 @@
 import * as React from "react";
 import { WebView } from "react-native-webview";
 
+const INJECTED_JAVASCRIPT = `
+  window.addEventListener("message", Subscribe);
+  document.addEventListener("message", Subscribe);
+
+  function Subscribe(event){
+    const json = parse(event);
+    if (json?.source !== "readyplayerme") {
+      return;
+    }
+
+    // Susbribe to all events sent from Ready Player Me once frame is ready
+    if (json.eventName === "v1.frame.ready") {
+      window.postMessage(
+        JSON.stringify({
+          target: "readyplayerme",
+          type: "subscribe",
+          eventName: "v1.**",
+        }),
+        "*"
+      );
+    }
+  }
+
+  function parse(event) {
+    try {
+      return JSON.parse(event.data);
+    } catch (error) {
+      return null;
+    }
+  }`;
+
 export default class App extends React.Component {
   render() {
     return (
       <WebView
-        source={{ uri: "https://demo.readyplayer.me/avatar?frameapi" }} // Change the source uri to your custom subdomain
+        source={{ uri: "https://harrison-test.readyplayer.me/avatar?frameApi" }} // Change the source uri to your custom subdomain
         onMessage={(event) => {
-          const json = parse(event);
-
-          if (json?.source !== "readyplayerme") {
-            return;
-          }
-
-          // Susbribe to all events sent from Ready Player Me once frame is ready
-          if (json.eventName === "v1.frame.ready") {
-            frame.contentWindow.postMessage(
-              JSON.stringify({
-                target: "readyplayerme",
-                type: "subscribe",
-                eventName: "v1.**",
-              }),
-              "*"
-            );
-          }
-
-          // Get avatar GLB URL
-          if (json.eventName === "v1.avatar.exported") {
-            console.log(`Avatar URL: ${json.data.url}`);
-            document.getElementById(
-              "avatarUrl"
-            ).innerHTML = `Avatar URL: ${json.data.url}`;
-            document.getElementById("frame").hidden = true;
-            alert(`Avatar 3D model can be downloaded from: ${json.data.ul}`);
-          }
-
-          // Get user id
-          if (json.eventName === "v1.user.set") {
-            console.log(
-              `User with id ${json.data.id} set: ${JSON.stringify(json)}`
-            );
-          }
-
-          function parse(event) {
-            try {
-              return JSON.parse(event.data);
-            } catch (error) {
-              return null;
-            }
+          var data = JSON.parse(event.nativeEvent.data);
+          if (data.readyPlayerMe?.name == "avatar.exported") {
+            alert(`${data.readyPlayerMe?.data.avatarUrl}`);
           }
         }}
+        javaScriptEnabled
+        injectedJavaScriptBeforeContentLoaded={INJECTED_JAVASCRIPT}
       />
     );
   }
