f1bbc7a47ccda84c46ec6b8cbc4bfb4faeb17824
Harrison <harrisonhough4@gmail.com>
2022-03-25 15:05:32 +0200

    refactored with new onLoad approach

--- App.js
+++ App.js
@@ -1,54 +1,105 @@
-import * as React from "react";
+//import * as React from "react";
+
+import React, { useRef } from "react";
 import { WebView } from "react-native-webview";
 
 const subdomain = "harrison-test"; // Replace with your custom subdomain
 
-const INJECTED_JAVASCRIPT = `
-  window.addEventListener("message", Subscribe);
-  document.addEventListener("message", Subscribe);
+let isSubscribed = false;
+let count = 0;
+const correlationId = "a0bf9c2a-44d7-4882-8e72-4bc7ab73849f";
 
-  function Subscribe(event){
-    const json = parse(event);
-    if (json?.source !== "readyplayerme") {
+function OnAvatarExported(json) {
+  console.log(JSON.stringify(json));
+  alert(`Avatar Url = ${json.data?.url}`);
+}
+
+const View = () => {
+  const webview = useRef();
+  const subscribe = () => {
+    if (isSubscribed) {
       return;
     }
 
-    // Susbribe to all events sent from Ready Player Me once frame is ready
-    if (json.eventName === "v1.frame.ready") {
-      window.postMessage(
-        JSON.stringify({
+    isSubscribed = true;
+    console.log(
+      `[PARENT] [OUTGOING] [${new Date().toISOString()}]`,
+      JSON.stringify(
+        {
           target: "readyplayerme",
           type: "subscribe",
-          eventName: "v1.**",
-        }),
-        "*"
-      );
-    }
-  }
+          eventName: "v1.avatar.exported",
+        },
+        null,
+        2
+      )
+    );
+    webview.current.postMessage(
+      JSON.stringify({
+        target: "readyplayerme",
+        type: "subscribe",
+        eventName: "v1.avatar.exported",
+      })
+    );
+  };
 
-  function parse(event) {
-    try {
-      return JSON.parse(event.data);
-    } catch (error) {
-      return null;
+  const process = (data) => {
+    const json = JSON.parse(data);
+    console.log(
+      `[PARENT] [INCOMING] [${new Date().toISOString()}]`,
+      JSON.stringify(json, null, 2)
+    );
+
+    if (json.source !== "readyplayerme") {
+      return;
     }
-  }`;
+
+    if (json.eventName === "v1.avatar.exported") {
+      OnAvatarExported(json);
+    }
+
+    if (json.eventName !== "v1.subscription.deleted") {
+      count++;
+
+      if (count > 4) {
+        console.log(
+          `[PARENT] [OUTGOING] [${new Date().toISOString()}]`,
+          JSON.stringify(
+            {
+              target: "readyplayerme",
+              type: "unsubscribe",
+              correlationId,
+            },
+            null,
+            2
+          )
+        );
+        webview.current.postMessage(
+          JSON.stringify({
+            target: "readyplayerme",
+            type: "unsubscribe",
+            correlationId,
+          })
+        );
+      }
+    }
+  };
+
+  return (
+    <WebView
+      ref={webview}
+      source={{
+        uri: `https://${subdomain}.readyplayer.me/avatar?frameApi`,
+      }} // Change the source uri to your custom subdomain
+      style={{ marginTop: 20 }}
+      onLoad={subscribe}
+      onMessage={(message) => process(message.nativeEvent.data)}
+    />
+  );
+};
 
 export default class App extends React.Component {
   render() {
-    return (
-      <WebView
-        source={{
-          uri: `https://${subdomain}.readyplayer.me/avatar?frameApi`,
-        }} // Change the source uri to your custom subdomain
-        injectedJavaScriptBeforeContentLoaded={INJECTED_JAVASCRIPT}
-        onMessage={(event) => {
-          var data = JSON.parse(event.nativeEvent.data);
-          if (data.readyPlayerMe?.name == "avatar.exported") {
-            alert(`${data.readyPlayerMe?.data.avatarUrl}`);
-          }
-        }}
-      />
-    );
+    return <View />;
   }
 }
