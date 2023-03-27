7c31fdbb9d0a4b0df7bc1b9ab6abc9fab3caa3d8
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-03-27 23:16:40 +0300

    feat: removed unsub event. cleanup.

--- App.js
+++ App.js
@@ -1,78 +1,46 @@
 import React, { useRef } from "react";
-import { WebView } from "react-native-webview";
+import WebView from 'react-native-webview';
 
 // Replace with your custom subdomain
 const subdomain = "demo";
 
-let isSubscribed = false;
-let count = 0;
-const correlationId = "a0bf9c2a-44d7-4882-8e72-4bc7ab73849f";
+export default function App() {
+  const webView = useRef();
+  
+  function onAvatarUrlReceived(message) {
+    alert(`Avatar Url = ${message.data?.url}`);
+  }
 
-function onAvatarExported(message) {
-  alert(`Avatar Url = ${message.data?.url}`);
-}
-
-const View = () => {
-  const webview = useRef();
-
-  const subscribe = () => {
-    if (isSubscribed) {
-      return;
-    }
-
-    isSubscribed = true;
-    webview.current.postMessage(
+  function onWebViewLoaded() {
+    webView.current.postMessage(
       JSON.stringify({
         target: "readyplayerme",
         type: "subscribe",
         eventName: "v1.avatar.exported",
       })
     );
-  };
+  }
 
-  const process = (data) => {
+  function onMessageReceived(message) {
+    const data = message.nativeEvent.data;
     const json = JSON.parse(data);
-
-    // Filter for only Ready Player Me Events
-    if (json.source !== "readyplayerme") {
+  
+    if (json?.source !== 'readyplayerme') {
       return;
     }
-
+  
     if (json.eventName === "v1.avatar.exported") {
-      // Event called after avatar has been created and the URL generated
-      onAvatarExported(json);
+      onAvatarUrlReceived(json);
     }
-
-    if (json.eventName !== "v1.subscription.deleted") {
-      count++;
-
-      if (count > 4) {
-        webview.current.postMessage(
-          JSON.stringify({
-            target: "readyplayerme",
-            type: "unsubscribe",
-            correlationId,
-          })
-        );
-      }
-    }
-  };
+  }
 
   return (
-    <WebView
-      ref={webview}
-      source={{
-        uri: `https://${subdomain}.readyplayer.me/avatar?frameApi`,
-      }}
-      style={{ marginTop: 20 }}
-      onLoad={subscribe}
-      onMessage={(message) => process(message.nativeEvent.data)}
+    <WebView 
+      ref={webView}
+      style={{ marginTop: 30 }}
+      onLoad={onWebViewLoaded}
+      onMessage={onMessageReceived}
+      source={{ uri: `https://${subdomain}.readyplayer.me/avatar?frameApi` }}
     />
   );
-};
-
-export default class App extends React.Component {
-  render() {
-    return <View />;
-  }
 }
